519828603b9a6d8cc896e79acc7f5f679823d518
H3nRyL <haoying97@gmail.com>
2021-04-27 23:29:19 -0700

    modified register.js so that users receive notification emails upon successful registration

--- utilities/email.js
+++ utilities/email.js
@@ -1,3 +1,5 @@
+var nodemailer = require('nodemailer');
+
 let sendEmail = (sender, receiver, subject, message) => {
     //research nodemailer for sending email from node.
     // https://nodemailer.com/about/
@@ -5,6 +7,29 @@ let sendEmail = (sender, receiver, subject, message) => {
     //create a burner gmail account 
     //make sure you add the password to the environmental variables
     //similar to the DATABASE_URL and PHISH_DOT_NET_KEY (later section of the lab)
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: sender,
+            pass: 'tcss450project'
+    }
+    });
+
+    var mailOptions = {
+        from: sender,
+        to: receiver,
+        subject: subject,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 
     //fake sending an email for now. Post a message to logs. 
     console.log("*********************************************************")
@@ -14,7 +39,6 @@ let sendEmail = (sender, receiver, subject, message) => {
     console.log("_________________________________________________________")
     console.log(message)
     console.log("*********************************************************")
-
 }
 
 module.exports = { 
